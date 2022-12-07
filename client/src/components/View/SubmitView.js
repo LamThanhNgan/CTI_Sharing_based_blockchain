358aac993b9cf39231ad855c44c22979c09b41f9
LamThanhNgan <lamthanhngan662607004@gmail.com>
2022-12-07 09:52:24 -0800

    test

---
+++ client/src/components/View/SubmitView.js
@@ -0,0 +1,19 @@
+/**
+ *    SPDX-License-Identifier: Apache-2.0
+ */
+
+ import React from 'react';
+ import View from '../Styled/View';
+ import SubmitForm from '../Forms/SubmitForm';
+
+ 
+
+ export const SubmitView = () => (
+   <View>
+        <SubmitForm />
+   </View>
+ );
+ 
+ 
+ export default SubmitView;
+ 
